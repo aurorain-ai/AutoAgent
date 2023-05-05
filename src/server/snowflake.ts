fb11f47247d1ea84dc090c10f293b169fb40314f
Michael Hu <michaelhu286@gmail.com>
2023-05-05 14:42:44 -0700

    Fixed Snowflake timeout disconnection
    Fixed Snowflake timeout disconnection.
    
    Test locally but wait to verify after timeout.

--- src/server/snowflake.ts
+++ src/server/snowflake.ts
@@ -13,65 +13,55 @@ const connectionConfig = {
   schema: process.env.SNOWFLAKE_SCHEMA ?? '',
 };
 
-let connection: snowflake.Connection;
+class SnowflakeConnection {
+  private connection: snowflake.Connection;
 
-function createConnection() {
-  const newConnection = snowflake.createConnection(connectionConfig);
-  newConnection.connect((err) => {
-    if (err) {
-      console.error("Unable to connect to Snowflake:", err);
-    } else {
-      console.log("Successfully connected to Snowflake.");
-    }
-  });
-  return newConnection;
-}
+  constructor() {
+    this.connection = this.createConnection();
+  }
 
-connection = createConnection();
-
-async function reconnect() {
-  return new Promise<void>((resolve, reject) => {
-    const newConnection = createConnection();
+  private createConnection(): snowflake.Connection {
+    const newConnection = snowflake.createConnection(connectionConfig);
     newConnection.connect((err) => {
       if (err) {
-        console.error('Failed to reconnect Snowflake:', err);
-        reject(err);
+        console.error("Unable to connect to Snowflake:", err);
       } else {
-        console.log('Reconnected Snowflake successfully.');
-        connection = newConnection;
-        resolve();
+        console.log("Successfully connected to Snowflake.");
       }
     });
-  });
-}
+    return newConnection;
+  }
 
-export async function querySnowflake(sqlText: string, retryCount: number = 0): Promise<any> {
-  return new Promise(async (resolve, reject) => {
-    connection.execute({
-      sqlText,
-      complete: async (err, stmt, rows) => {
-        if (err) {
-          const snowflakeError = err as SnowflakeError;
-          console.error('querySnowflake error:', err);
-          // 407001: a connection was never established.
-          // 407002: a connection was disconnected.
-          if ((snowflakeError.code === 407002 || snowflakeError.code === 407001) && retryCount < 3) {
-            console.error('Connection terminated. Attempting to reconnect...');
-            try {
-              await reconnect();
-              resolve(await querySnowflake(sqlText, retryCount + 1));
-            } catch (reconnectError) {
-              reject(reconnectError);
+  public async execute(sqlText: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+      this.connection.execute({
+        sqlText,
+        complete: (err, stmt, rows) => {
+          if (err) {
+            const snowflakeError = err as SnowflakeError;
+            console.error('querySnowflake error:', err);
+            if (snowflakeError.code === 407002 || snowflakeError.code === 407001) {
+              console.error('Connection terminated. Attempting to reconnect...');
+              this.connection = this.createConnection();
+              this.execute(sqlText)
+                .then(resolve)
+                .catch(reject);
+            } else {
+              reject(err);
             }
           } else {
-            reject(err);
+            resolve(rows);
           }
-        } else {
-          resolve(rows);
-        }
-      },
+        },
+      });
     });
-  });
+  }
+}
+
+const snowflakeConnection = new SnowflakeConnection();
+
+export async function querySnowflake(sqlText: string): Promise<any> {
+  return snowflakeConnection.execute(sqlText);
 }
 
 export async function snowflakeHandler(req: Request, res: Response): Promise<void> {
