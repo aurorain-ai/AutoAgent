9b5671ae706877d056c110a7eb2015a0934ec580
Michael Hu <michaelhu286@gmail.com>
2023-05-14 23:06:43 -0700

    Measure and optimize the execution logic
    Ask: generate a complicated but short query for testing with complexity_level 3
    Tuned SQL: SELECT O_ORDERKEY, O_ORDERDATE, O_TOTALPRICE, L_PARTKEY, L_EXTENDEDPRICE, C_NAME, S_NAME, P_BRAND FROM ORDERS O JOIN LINEITEM L ON O_ORDERKEY = L_ORDERKEY JOIN CUSTOMER C ON O_CUSTKEY = C_CUSTKEY JOIN SUPPLIER S ON L_SUPPKEY = S_SUPPKEY JOIN PART P ON L_PARTKEY = P_PARTKEY WHERE O_ORDERDATE < DATE '1994-09-01' AND L_EXTENDEDPRICE > 1000 AND P_BRAND = 'Brand#12' LIMIT 10;
    Measured results:
    Running sqlTuneAgent:  1
    AgentService.sqlTuneAgent1: 1:37.789 (m:ss.mmm)
    querySnowflakeAPI1: 1.231s
    Running sqlTuneAgent:  2
    AgentService.sqlTuneAgent1: 42.574s
    Running sqlTuneAgent:  3
    AgentService.sqlTuneAgent1: 25.821s
    querySnowflakeAPI3: 2.597s
    
    Simple query----
    Ask: select count from customer
    Running sqlTuneAgent:  1
    AgentService.sqlTuneAgent1: 7.450s
    querySnowflakeAPI3: 502.677ms

--- src/pages/api/agent/start.ts
+++ src/pages/api/agent/start.ts
@@ -5,8 +5,8 @@ import AgentService from "../../../services/agent-service";
 import { querySnowflakeAPI } from '../../../services/snowflake-service';
 import { getQueryOperatorStats } from "../../../utils/query-opstats";
 
-const MAX_TUNING: number = process.env.LLM_SQL_TUNING_MAX_TIMES && !isNaN(parseInt(process.env.LLM_SQL_TUNING_MAX_TIMES)) ? parseInt(process.env.LLM_SQL_TUNING_MAX_TIMES) : 3;
-const COMP_LEVEL_THRESHOLD: number = process.env.SQL_COMPLEXITY_LEVEL_THRESHOLD && !isNaN(parseInt(process.env.SQL_COMPLEXITY_LEVEL_THRESHOLD)) ? parseInt(process.env.SQL_COMPLEXITY_LEVEL_THRESHOLD) : 3;
+const MAX_TUNING: number = process.env.LLM_SQL_TUNING_MAX_TIMES ? parseInt(process.env.LLM_SQL_TUNING_MAX_TIMES) : 3;
+const COMP_LEVEL_THRESHOLD: number = process.env.SQL_COMPLEXITY_LEVEL_THRESHOLD ? parseInt(process.env.SQL_COMPLEXITY_LEVEL_THRESHOLD) : 3;
 
 
 export const config = {
@@ -24,43 +24,56 @@ const handler = async (request: NextRequest) => {
     console.log("startHanlder statement:", goal);
 
     // step 2: fine-tune SQL for understanding ops
-    var num = 0;
-    var sqlStmt;
-    var pruning_result;
-    var comp_level = COMP_LEVEL_THRESHOLD;
+    let num = 0;
+    let sqlStmt;
+    let pruning_result;
+    let comp_level = COMP_LEVEL_THRESHOLD;
     while (num++ < MAX_TUNING && comp_level >= COMP_LEVEL_THRESHOLD) {
       console.log("Running sqlTuneAgent: ", num);
+      console.time("AgentService.sqlTuneAgent1");
       const t_res = await AgentService.sqlTuneAgent(modelSettings, goal, pruning_result);
+      console.timeEnd("AgentService.sqlTuneAgent1");
       console.log("AgentService.sqlTuneAgent:", t_res);
       sqlStmt = t_res.main_SQL;
       comp_level = t_res.complexity_level;
       if (comp_level >= COMP_LEVEL_THRESHOLD && t_res.pruning_SQL) {
         console.log("sqlTuneAgent: query pruning_SQL: ", t_res.pruning_SQL);
+        console.time("querySnowflakeAPI1");
         pruning_result = await querySnowflakeAPI(t_res.pruning_SQL)
+        console.timeEnd("querySnowflakeAPI1");
       }
     }
 
     // step 3: query snowflake
-    var data = await querySnowflakeAPI(sqlStmt);
+    console.time("querySnowflakeAPI3");
+    let data = await querySnowflakeAPI(sqlStmt);
+    console.timeEnd("querySnowflakeAPI3");
     console.log("querySnowflakeAPI return:", data);
 
     // step 4: Fine-tune SQL again for query failures
-    if (data !== null && typeof data === 'object' && 'error' in data) {
+    if (data && typeof data === 'object' && data.error) {
       console.error("Fine-tuning for querySnowflakeAPI error: ", data.error);
 
       // step 4.1 get_query_operator_stats
       const queryOpsStats = await getQueryOperatorStats();
 
       // step 4.2: fine-tune SQL from LLM
-      var newSQLStmt = await AgentService.sqlQueryAgent(modelSettings, goal, sqlStmt, data.error, queryOpsStats);
+      console.time("AgentService.sqlQueryAgent1");
+      let newSQLStmt = await AgentService.sqlQueryAgent(modelSettings, goal, sqlStmt, data.error, queryOpsStats);
+      console.timeEnd("AgentService.sqlQueryAgent1");
       console.log("AgentService.sqlQueryAgent fine tuned:", newSQLStmt);
 
-      if (newSQLStmt.trim().toLowerCase() === sqlStmt.trim().toLowerCase()) {
+      const newSQL = newSQLStmt.trim().toLowerCase();
+      const oldSQL = sqlStmt.trim().toLowerCase();
+
+      if (newSQL === oldSQL) {
         console.warn("Same fine-tuned SQL statement, skipping it.");
       }
       else {
         // step 4.3: query snowflake again
+        console.time("querySnowflakeAPI5");
         data = await querySnowflakeAPI(newSQLStmt);
+        console.timeEnd("querySnowflakeAPI5");
         console.log("querySnowflakeAPI fine-tuned return:", data);
         sqlStmt = newSQLStmt;
       }
